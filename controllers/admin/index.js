0cf600799a5067591bc6d684253cafc9e8c370ec
PhilTYYC <60204986+PhilTYYC@users.noreply.github.com>
2024-11-10 02:06:04 -0700

    task: some clean up

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 router.route('/')
 .get((req, res) => {
     console.log('admin');
-    res.send('this is admin route');
+    res.send('admin route');
 });
 
 router.route('/login')
 .get((req, res) => {
     console.log('admin/login');
-    res.send(`this is admin login route, ${req}`);
+    res.send(`admin login route, ${req}`);
 });
 
 
--- controllers/users/index.js
+++ controllers/users/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 router.route('/')
     .get((req, res) => {
         console.log('users');
-        res.send(`this is users route, ${req}`);
+        res.send(`users route, ${req}`);
     });
 router.route('/list')
     .get((req, res) => {
         console.log('users/list');
-        res.send(`this is users list route, ${req}`);
+        res.send(`users list route, ${req}`);
     });
 
 module.exports = router;
